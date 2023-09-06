aa540d14c06524974211bd32eb777b29578b3420
Lovis Rentsch <gpg@lovirent.eu>
2023-09-06 16:45:14 +0200

    hello

--- default.nix
+++ default.nix
@@ -1,4 +1,15 @@
-let 
-   sources = import ./nix/sources.nix;
+let
+  sources = import ./nix/sources.nix;
 
-in sources.nixpkgs
+  hello = pkgs.writeShellScriptBin "hello" ''
+    echo "Hello from the Nix channel overlay!"
+  '';
+
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (self: super: {
+        inherit hello;
+      })
+    ];
+  };
+in pkgs
